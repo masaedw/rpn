82fba7665b977d3e6127a0d6285345f866ff9566
masaedw <masa.edw@gmail.com>
2013-01-24 12:00:41 +0900

    conduit runner

--- rpn.hs
+++ rpn.hs
@@ -1,15 +1,23 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 --import Data.Conduit.Attoparsec
-import Control.Applicative ((<|>),(*>),(<$>),pure)
+import Control.Applicative ((<|>),(*>),(<*),(<$>),pure)
 import Data.Attoparsec.ByteString (Parser)
 import qualified Data.Attoparsec.ByteString as A
 import qualified Data.Attoparsec.Char8 as AC
 import qualified Data.ByteString.Char8 as B
+import qualified Data.Conduit as C
+import qualified Data.Conduit.Binary as CB
+import qualified Data.Conduit.List as CL
+import Data.Conduit (($=),($$))
+import System.IO
 
 main :: IO ()
-main = do
-  return ()
+main = C.runResourceT
+       $ CB.sourceHandle stdin
+       $= CB.lines
+       $= CL.map ((`B.append` "\n") . B.pack . show . AC.parseOnly rpn)
+       $$ CB.sinkHandle stdout
 
 data RPN = Plus
          | Minus
@@ -19,7 +27,7 @@ data RPN = Plus
          deriving (Show, Eq)
 
 rpn :: Parser [RPN]
-rpn = (rpnValue <|> rpnOp) `A.sepBy1` A.many' AC.space
+rpn = (rpnValue <|> rpnOp) `A.sepBy1` A.many' AC.space <* AC.endOfInput
 
 rpnOp :: Parser RPN
 rpnOp = ops [("+", Plus)
